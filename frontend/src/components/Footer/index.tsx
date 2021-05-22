b165ba03f042697c02dbcb3dda68c4c511167a05
Felipe <1felipersilva1@gmail.com>
2021-05-21 22:31:56 -0300

    basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://github.com/felipe-rodrigues-s"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Felipe Rodrigues
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://www.instagram.com/felipersilvarsilva/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @felipersilvarsilva.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'assets/css/styles.css'; 
-import App from './App';
-
+//node Module
+import React from "react";
+import ReactDOM from "react-dom";
+//styles
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/css/styles.css";
+//componetes
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
